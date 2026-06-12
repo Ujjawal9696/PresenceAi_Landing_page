8bd4a33f4fa156aa1a5c21c8199f43a0b8980428
Ujjawal <ujjawal122004@gmail.com>
2026-06-12 13:10:34 +0530

    Fix: correct streamlit URL

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 # Set your deployed Streamlit app URL here once
-APP_URL = os.environ.get("APP_URL", "https://presenceai.streamlit.app/")
-
+APP_URL = os.environ.get("APP_URL", "https://presenceaiproject-ckz6jmjs33upmemmqwmbyq.streamlit.app/")
 @app.route('/')
 def index():
     return render_template('index.html', app_url=APP_URL)
